dcfe56473131ce7a438061d46fdd7ea97ff011b1
oliver <380425169@qq.com>
2018-01-18 22:19:06 +0800

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="home">
+		ddddddd
+		<div class="header_img">
+			<img src="src/img/header_img.jpg" alt="">
+		</div>
+		<div id="banner">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide"></div>
+				</div>
+				<div class="swiper-pagination"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+	
+</style>
